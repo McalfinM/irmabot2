a2666def6c9d6ad5c50783bcd60b9706eb8b65ac
werich1 <47493331+werich1@users.noreply.github.com>
2019-02-11 09:24:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ copyright @ medantechno.com
 
 require_once('./line_class.php');
 
-$channelAccessToken = 'AW6Vkxbg5p+7WyHScq6vO1orZjZtb3L6HgKd5m0foonvQOnlHNdXgDOcTUQp9QNNB16I/woC7gL/UU5ET'; //sesuaikan 
-$channelSecret = '40ec532da1b02e850e0e496609472381';//sesuaikan
+$channelAccessToken = 'r3sxvkn3C6W/zGhhYJopXhWyv50STxlSPTsChXViPfhtGXkACyjSFpeg+XVnJikZnTiUXT7G/FXKPZQmU5Mo1A10hr6IhSLzsiT8M+V8nRBKH09GzG0cNlrIKvGP3w/0iPqzEvGFtqG7iE9PrFhBIwdB04t89/1O/w1cDnyilFU='; //sesuaikan 
+$channelSecret = '1aafe6a16e658347129e845070736c66';//sesuaikan
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 $userId 	= $client->parseEvents()[0]['source']['userId'];
